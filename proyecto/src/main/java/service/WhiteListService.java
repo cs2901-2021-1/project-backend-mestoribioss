246a475aa9da22d7447c9f16d8b3572e0485c50c
DiegoOrtizS <52261652+DiegoOrtizS@users.noreply.github.com>
2021-07-10 13:14:18 -0500

    add y delete en white list

--- proyecto/src/main/java/service/WhiteListService.java
+++ proyecto/src/main/java/service/WhiteListService.java
@@ -18,15 +18,25 @@ public class WhiteListService {
 
     // Añadir a la tabla usuario para que tenga permiso
     public User save(UserDTO userDTO){
-        
+        var user = new User();
+        user.setLastName(userDTO.getLastName());
+        user.setName(userDTO.getName());
+        user.setRole(userDTO.getRole());
+        user.setStatus(userDTO.getStatus());
+        user.setEmail(userDTO.getEmail());
+        return userRepository.save(user);
     }
 
     // Quitarle los permisos a un usuario de la whitelist
     public void deleteById(Long id){
-
+        User user = findOneById(id);
+        user.setStatus(0);
+        userRepository.save(user);
     }
 
     public User findOneById(Long id){
-
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()) return userOptional.get();
+        else throw new CustomNotFoundException("¡El usuario con el id " + id + " no existe!");
     }
 }
