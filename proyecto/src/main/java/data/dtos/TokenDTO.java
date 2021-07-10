a4f3457bf0a55dd52690f6ef4c03ad0be799c848
jorgevasquezutec <jorge.vasquez@utec.edu.pe>
2021-07-10 15:57:07 -0500

    OAuhtCOntroller

---
+++ proyecto/src/main/java/data/dtos/TokenDTO.java
@@ -0,0 +1,15 @@
+package data.dtos;
+
+public class TokenDTO {
+
+    String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+}
